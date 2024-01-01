65e5f6b4a2e0bfb6147af953efa538c86e0f0f93
Eysteix <153673423+Eysteix@users.noreply.github.com>
2024-01-01 11:39:46 +0000

    Update client-config.ts

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,7 +2,8 @@ const config ={
         projectId : 'ktjlfdyf',
         dataset : 'production',
         apiVersion: '2023-12-22',
-        useCdn:false
+        token:'sku25XTrR9CwLUHn5nooGpj9zR83pI5TsbV4CP3l734VVl632wePFEJVOGz3lVvjNjyjaSWzUvVMmkcyi4QHBhvtXg12klkUj6FrVm8EreBrJQCUuMIbWjlcq46kktMQEwlWgLmtEIL9JcLUzSORJkbQQdBis5b7I3tH7YgYd332r5PxPZNt',
+        useCdn:true
 }
 
 
